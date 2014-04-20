5a83db0de75a6e9105288b42d2c0f7fb0f72e7d6
Xavier Xu <xavier.xu@msn.com>
2014-04-20 13:34:45 -0700

    Modified my version of TCP. The server parses the input from client side and handles errors appropriately.

--- TCPClient.java
+++ TCPClient.java
@@ -6,47 +6,62 @@ public class TCPClient {
 
         String serverHostname = new String ("127.0.0.1");
         int port = 10007;
-        if (args.length > 0)
-           serverHostname = args[0];
-        if (args.length >= 1)
-            port = Integer.parseInt(args[1]);
-        System.out.println ("Attemping to connect to host " +
-		serverHostname + " on port" + port + ".");
+        long timestart = System.currentTimeMillis();
 
+        if (args.length > 0) 
+            serverHostname = args[0];
+        if (args.length > 1) 
+            port = Integer.parseInt(args[1]);
+        System.out.println ("\nAttemping to connect to host " +
+                serverHostname + " on port: " + port + " at " +
+                (System.currentTimeMillis()-timestart) + " milliseconds");
         Socket echoSocket = null;
         PrintWriter out = null;
         BufferedReader in = null;
 
         try {
-            // echoSocket = new Socket("taranis", 7);
             echoSocket = new Socket(serverHostname, port);
             out = new PrintWriter(echoSocket.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(
-                                        echoSocket.getInputStream()));
+                    echoSocket.getInputStream()));
         } catch (UnknownHostException e) {
             System.err.println("Don't know about host: " + serverHostname);
             System.exit(1);
         } catch (IOException e) {
             System.err.println("Couldn't get I/O for "
-                               + "the connection to: " + serverHostname);
+                    + "the connection to: " + serverHostname);
             System.exit(1);
         }
 
-	BufferedReader stdIn = new BufferedReader(
-                                   new InputStreamReader(System.in));
-	String userInput;
+        BufferedReader stdIn = new BufferedReader(
+                new InputStreamReader(System.in));
+        String userInput;
 
-        System.out.print ("input: ");
-	while ((userInput = stdIn.readLine()) != null) {
-	    out.println(userInput);
-	    System.out.println("echo: " + in.readLine());
-            System.out.print ("input: ");
-	}
+        System.out.println ("\nConnection successful! Key-Value Store is ready! Available operations:");
+        System.out.println ("1: PUT(Key, Value)");        
+        System.out.println ("2: GET(Key)");
+        System.out.println ("3: DELETE(Key)");
+        System.out.println ("4: Exit");
+        
+        System.out.print ("\nCommand: ");
+        while ((userInput = stdIn.readLine()) != null) {
+            out.println(userInput);
+            if (in.readLine() == null) 
+            {
+                System.exit(1);
+            } 
+            else 
+            {
+                System.out.println("Server> " + in.readLine());
+            }
+            
+            System.out.print("\nCommand: ");
+        }
 
-	out.close();
-	in.close();
-	stdIn.close();
-	echoSocket.close();
+        out.close();
+        in.close();
+        stdIn.close();
+        echoSocket.close();
     }
 }
 
--- TCPServer.java
+++ TCPServer.java
@@ -28,39 +28,93 @@ public class TCPServer
 
         try { 
             clientSocket = serverSocket.accept(); 
-            System.out.println(clientSocket);
+            //            System.out.println(clientSocket);
         } 
         catch (IOException e) 
         { 
-            System.err.println("Accept failed."); 
+            System.err.println("Accept failed at " + (System.currentTimeMillis() - timestart) 
+                    + " milliseconds on port " + port + "."); 
             System.exit(1); 
         } 
 
-        System.out.println ("Connection successful");
-        
-        // Setup bytes for sending and receiving packets
-        byte[] receiveOperationData = new byte[1024];
-        byte[] receiveKeyData = new byte[1024];
-        byte[] receiveValueData = new byte[1024];
-        byte[] sendData = new byte[1024];
-        
-        
-        
-        
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), 
-                true); 
+
+
+        InetAddress IPAddress = clientSocket.getInetAddress();
+        int clientPort = clientSocket.getLocalPort();
+
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true); 
         BufferedReader in = new BufferedReader( 
                 new InputStreamReader( clientSocket.getInputStream())); 
 
         String inputLine; 
-
-        while ((inputLine = in.readLine()) != null) 
+        
+//        while ((inputLine = in.readLine()) != null) 
+        while (true) 
         { 
-            System.out.println ("Server: " + inputLine); 
-            out.println(inputLine); 
+            inputLine = in.readLine().trim();
 
-            if (inputLine.equals("Bye.")) 
-                break; 
+            //            inputLine = inputLine.trim();
+            String returnMessage = "Invalid request received from " + IPAddress + ":" +
+                    clientPort + " at " + (System.currentTimeMillis()-timestart) +
+                    " milliseconds! Please try again!";
+
+            if (inputLine.endsWith(")")) {
+                inputLine = inputLine.substring(0, inputLine.length() - 1); 
+                String[] inputSegs = inputLine.split("[(]");
+                String operation = inputSegs[0];
+                String key = inputSegs[1];
+                if (operation.equalsIgnoreCase("get")) 
+                {
+
+                    if (store.containsKey(key)) 
+                    {
+                        returnMessage = "Value \"" + store.get(key) + "\" for Key \"" + key +
+                                "\" returned to " + IPAddress + ":" + clientPort + " at " +
+                                (System.currentTimeMillis()-timestart) + " milliseconds";
+                    }
+                    else
+                    {
+                        returnMessage = "No value found in store for Key \"" + key + "\" at " +
+                                (System.currentTimeMillis()-timestart) + " milliseconds";
+                    }
+                } 
+                else if (operation.equalsIgnoreCase("delete")) 
+                {
+
+                    if (store.containsKey(key))
+                    {
+                        returnMessage = "Key \"" + key + "\" Value \"" + store.get(key) +
+                                "\" deleted at " + (System.currentTimeMillis()-timestart) + " milliseconds";
+                        store.remove(key); // delete key/value from the Map
+                    }
+                    else
+                    {
+                        returnMessage = "No value found in store for Key \"" + key + "\" at " +
+                                (System.currentTimeMillis()-timestart) + " milliseconds";
+                    }
+                }
+                else if (operation.equalsIgnoreCase("put") && key.contains(",")) 
+                {
+                    String[] keySegs = key.split(",");
+                    key = keySegs[0].trim();
+                    String value = keySegs[1].trim();
+                    store.put(key, value); // place key/value into the Map
+
+                    returnMessage = "Key \"" + key + "\" Value \"" + store.get(key) + "\" stored at " + (System.currentTimeMillis()-timestart) + " milliseconds";
+                }
+
+                System.out.println ("Server: " + returnMessage); 
+                out.println(returnMessage); 
+            }
+            else if (inputLine.equals("Exit")) 
+            {
+                break;
+            } else 
+            {
+                System.out.println ("Server: " + returnMessage); 
+                out.println(returnMessage); 
+            }
+
         } 
 
         out.close(); 
