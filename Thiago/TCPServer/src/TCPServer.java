47d8ec37397dd8e74052871c90f961ebabef188a
Thiago Henrique Deicke <deicke@live.com>
2014-04-20 23:07:52 -0700

    Final

--- Thiago/TCPServer/src/TCPServer.java
+++ Thiago/TCPServer/src/TCPServer.java
@@ -70,16 +70,13 @@ class TCPServer
 				// get value from value packet
 				if (operation.matches("put")) // put operation requires a third "value" packet
 				{
-					try{
-						value = dataScanner.next();
-					}catch(InputMismatchException e){
-						
-					}
-					value = value.trim();
-					if(value.isEmpty()){
+					if(!dataScanner.hasNext()){
 						System.out.println("Insufficient arguments at" + (System.currentTimeMillis()-timestart) + " milliseconds");
 						break;
 					}
+					value = dataScanner.next();
+					value = value.trim();
+					
 				}
 				
 				// if no value packet was received, value string will be empty and not obstruct the code to follow
